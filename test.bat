b2b0f6aad819a1eb5da508e71d4351020b2968ed
Le Anh Tuan <latuannetnam@gmail.com>
2026-01-10 19:23:24 +0700

    update style

--- test.bat
+++ test.bat
@@ -5,4 +5,5 @@
 @REM  Kiem Lai
 @REM uv run dich-truyen pipeline --url "https://www.piaotia.com/html/8/8717/index.html" --chapters 1-5 --format pdf
 @REM Ao Thuat Than Toa
-uv run dich-truyen pipeline --url https://www.piaotia.com/html/3/3759/index.html --chapters 1-5 --style tay_phuong --format pdf
+@REM uv run dich-truyen pipeline --url https://www.piaotia.com/html/3/3759/index.html --chapters 1-5 --style tay_phuong --format pdf
+uv run dich-truyen pipeline --book-dir .\books\3759-indexhtml\ --chapters 1-2 --style tay_phuong --format pdf --translate-only --force
