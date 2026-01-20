cd46efdc0ccd6a52d32bb8563f66f83c8fb0e5d0
Le Anh Tuan <latuannetnam@gmail.com>
2026-01-20 07:05:34 +0700

    Update translation guidelines and test script for consistency

--- test.bat
+++ test.bat
@@ -10,3 +10,4 @@
 
 @REM Dai Dao Tranh Phong
 uv run dich-truyen pipeline --url https://www.piaotia.com/html/2/2408/index.html --chapters 1-5 --format pdf --translate-only --force
+uv run dich-truyen pipeline --url https://www.piaotia.com/html/2/2408/index.html --translate-only --force
